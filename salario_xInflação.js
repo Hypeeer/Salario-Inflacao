2f2726154b6f86fe99c0b35a6b96255f8ec35525
Hypeeer <vittorjoao18@hotmail.com>
2024-06-14 18:06:52 -0300

    Refatoração

--- salario_xInflação.js
+++ salario_xInflação.js
@@ -1,28 +1,6 @@
-import options from 'readline-sync';
+import option from 'readline-sync';
 
-
-const dataFormat = {
-    hour: 'numeric',
-    minute: 'numeric',
-}
-
-const dataAtual = new Date();
-const hora = dataAtual.getHours();
-
-let msg = "";
-
-if (hora == 5 || hora < 12) {
-    msg += "Bom Dia!!"
-} else if (hora > 12 && hora < 18) {
-    msg += "Boa Tarde!!"
-}else{
-    msg += "Boa Noite!!"
-}
-
-const menuFormat = "";
-
-
-//coleção que junta salaro/ano com inflação referente a cada ano
+// coleção de salario, inflação e o ano
 let array_salarioMinimo = [
     { salario: 510.00, ano: 2010, infla: 5.92 },
     { salario: 545.00, ano: 2011, infla: 6.50 },
@@ -36,56 +14,83 @@ let array_salarioMinimo = [
     { salario: 998.00, ano: 2019, infla: 4.31 },
     { salario: 1045.00, ano: 2020, infla: 4.52 }
 ];
-//Menu que recebe interação do usuario
-console.log(`${menuFormat.padStart(40,"*")}`);
-console.log(`Bem Vindo, São ${dataAtual.toLocaleTimeString('pt-br',dataFormat)} ${msg}\n`);
-console.log("1 - Lista historico salario mínimo ");
-console.log("2 - Lista historico taxa IPCA ");
-console.log("3 - Lista de porcentagem de crecimento salario\n");
-console.log(`${menuFormat.padStart(40,"*")}`);
-let option = options.question("Digite sua opcao: ");
 
-switch (option) {
-    case "1":
-        //Lista o salario mínimo de cada ano
-        for (let lista of array_salarioMinimo) {
-            //Passando metodo padEnd dentro da templet literal 
-            console.log(`${"Ano: ".padEnd(30, ".")} ${lista.ano}`);
-            console.log(`${"Salario Minimo: ".padEnd(30, ".")} R$${lista.salario.toFixed(2).replace(".", ",")}\n`);
-        }
-        break;
-    case "2":
-        //Lista o ano e a porcentagem da inflação
-        for (let lista of array_salarioMinimo) {
-            console.log(`${"Ano: ".padEnd(30, ".")} ${lista.ano}`);
-            console.log(`${"Inflacao IPCA: ".padEnd(30, ".")} ${lista.infla.toFixed(2).replace(".", ",")}%\n`);
-        }
-        break;
-    case "3":
+// função do menu principal
+const menu_principal = () => {
 
-        let crecimentoSalarial;
-        //Exibe o salario a de 2010 sem o calculo incial
-        console.log(`${"Ano: ".padEnd(30, ".")} ${array_salarioMinimo[0].ano}`);
-        console.log(`${"Salario Mínimo".padEnd(30, ".")} R$${array_salarioMinimo[0].salario.toFixed(2).replace(".", ",")}`);
-        console.log(`${"Crecimento Salarial: ".padEnd(30, ".")} - `);
-        console.log(`${"Inflação IPCA: ".padEnd(30, ".")} ${array_salarioMinimo[0].infla.toFixed(2).replace(".", ",")}%\n`);
+    let formatacao = "";
 
-        for (let i = 1; i < array_salarioMinimo.length; i++) {
-            //Exibe o salario de 2011 em diante com calculo
-            const salariolAtual = array_salarioMinimo[i].salario;
-            const salarioAnterior = array_salarioMinimo[i - 1].salario;
-            const diferenca = salariolAtual - salarioAnterior;
-            crecimentoSalarial = (diferenca / salarioAnterior) * 100;
+    console.log(`${formatacao.padStart(50, "*")}`);
+    console.log(`Bem - Vindo (a)`);
+    console.log(`1 - Lista historico salario mínimo`);
+    console.log(`2 - Lista historico taxa IPCA`);
+    console.log(`3 - lista de porcentagem de crecimento salario`);
+    console.log(`${formatacao.padStart(50, "*")}`);
 
-            //Lista salario, inflação e crecimento salarial comparando com a inflação
-            console.log(`${"Ano: ".padEnd(30, ".")} ${array_salarioMinimo[i].ano}`);
-            console.log(`${"Salario Mínimo".padEnd(30, ".")} R$ ${array_salarioMinimo[i].salario.toFixed(2).replace(".", ",")}`);
-            console.log(`${"Crecimento Salarial: ".padEnd(30, ".")} ${crecimentoSalarial.toFixed(2).replace(".", ",")}%`);
-            console.log(`${"Inflação IPCA: ".padEnd(30, ".")} ${array_salarioMinimo[i].infla.toFixed(2).replace(".", ",")}%\n`);
-        }
-        break;
-    default:
-        //verificação de input
-        console.log("Opção Invalida!");
-        break;
 }
+
+// função para percorrer a coleção de objetos na propriedade salario e ano
+const lista_historico_salario_minimo = (arr) => {
+
+    for (let lista of arr) {
+
+        console.log(`${"Ano: ".padEnd(30, ".")} ${lista.ano}`);
+        console.log(`${"Salarios:".padEnd(30, ".")} ${lista.salario.toFixed(2).replace(".", ",")}\n`);
+
+    }
+}
+
+// funçao para percorre a coleção de objetos na propriedade inflação e ano
+const lista_historico_PCA = (arr) => {
+
+    for (let lista of arr) {
+
+        console.log(`${"Ano: ".padEnd(30, ".")} ${lista.ano}`);
+        console.log(`${"Inflação PCA: ".padEnd(30, ".")} ${lista.infla.toFixed(2).replace(".", ",")}%\n`);
+    }
+}
+
+// função para processamento dos dados indicando a diferença de salario a cada ano
+const porcentagem_crecimento_salarial = (arr) => {
+
+    let crecimentoSalarial;
+
+    for (let i = 1; i < arr.length; i++) {
+        //Exibe o salario de 2011 em diante com calculo
+        const salariolAtual = arr[i].salario;
+        const salarioAnterior = arr[i - 1].salario;
+        const diferenca = salariolAtual - salarioAnterior;
+        crecimentoSalarial = (diferenca / salarioAnterior) * 100;
+
+        //Lista salario, inflação e crecimento salarial comparando com a inflação
+        console.log(`${"Ano: ".padEnd(30, ".")} ${arr[i].ano}`);
+        console.log(`${"Salario Mínimo".padEnd(30, ".")} R$ ${arr[i].salario.toFixed(2).replace(".", ",")}`);
+        console.log(`${"Crecimento Salarial: ".padEnd(30, ".")} ${crecimentoSalarial.toFixed(2).replace(".", ",")}%`);
+        console.log(`${"Inflação IPCA: ".padEnd(30, ".")} ${arr[i].infla.toFixed(2).replace(".", ",")}%\n`);
+    }
+}
+
+// função para escolha do usuario no menu 
+const escolha_opcao = () => {
+
+    let options;
+    menu_principal();
+    options = option.question(`Escolha uma Opcao: `);
+    options = parseInt(options);
+
+    switch (options) {
+        case 1:
+            lista_historico_salario_minimo(array_salarioMinimo);
+            break;
+        case 2:
+            lista_historico_PCA(array_salarioMinimo);
+            break;
+        case 3:
+            porcentagem_crecimento_salarial(array_salarioMinimo)
+            break;
+        default:
+            console.log("Opcao invalida");
+    };
+}
+
+escolha_opcao()
